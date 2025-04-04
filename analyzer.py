fe1445ad84b2b6814154d74fb150cb7b538cb765
Dani <Dani@DANIPC>
2025-04-04 09:41:50 +0200

    Author headers updated

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,10 @@
 __author__ = "Dani Martinez"
-__copyright__ = "Copyright 2024, Moblanc Robotics & Cornell University"
+__copyright__ = "Copyright 2025, Moblanc Robotics & Cornell University"
 __credits__ = ["Dani Martinez"]
 __license__ = "Apache 2.0"
-__version__ = "1.0"
+__version__ = "0.5"
 __maintainer__ = "Dani Martinez"
-__email__ = "dmartinez@moblancrobotics.com"
+__email__ = "dani.martinez@moblancrobotics.com"
 __status__ = "Production"
 
 import time
